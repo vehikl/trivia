fbcab73ef33dfbf3a5e1bf51c5635050664c16aa
Dan Soppelsa <dsoppelsa@gmail.com>
2018-04-13 15:44:28 -0400

    Create lookup table to get current category

--- php/src/Game.php
+++ php/src/Game.php
@@ -93,16 +93,9 @@ class Game
 
     function currentCategory()
     {
-        if ($this->places[$this->currentPlayer] == 0) return "Pop";
-        if ($this->places[$this->currentPlayer] == 4) return "Pop";
-        if ($this->places[$this->currentPlayer] == 8) return "Pop";
-        if ($this->places[$this->currentPlayer] == 1) return "Science";
-        if ($this->places[$this->currentPlayer] == 5) return "Science";
-        if ($this->places[$this->currentPlayer] == 9) return "Science";
-        if ($this->places[$this->currentPlayer] == 2) return "Sports";
-        if ($this->places[$this->currentPlayer] == 6) return "Sports";
-        if ($this->places[$this->currentPlayer] == 10) return "Sports";
-        return "Rock";
+        return [
+            'Pop', 'Science', 'Sports', 'Rock'
+        ][$this->places[$this->currentPlayer] % 4];
     }
 
     function correctAnswer()
