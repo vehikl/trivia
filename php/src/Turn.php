4135a1edc2c0c0aabf77adcdf0067df942adb25b
Grant Lovell <grant.lovell@gmail.com>
2018-04-14 11:49:46 -0400

    move messages into view class

--- php/src/Turn.php
+++ php/src/Turn.php
@@ -16,39 +16,23 @@ class Turn
         $this->roll = $roll;
         $this->game = $game;
 
-        $this->view = new TurnView($this);
+        $this->view = new TurnView($this->player);
     }
 
     public function move()
     {
         $this->player->setRoll($this->roll);
-        $this->displayPlayerRolls($this->roll->getValue());
+        $this->view->displayPlayerRolls($this->roll->getValue());
 
         if ($this->player->isNotAllowedToMove()) {
-            return $this->displayPlayerStaysInPenaltyBox();
+            return $this->view->displayPlayerStaysInPenaltyBox();
         }
 
         if ($this->player->isInPenaltyBox()) {
-            $this->displayPlayerGetsOutOfPenaltyBox();
+            $this->view->displayPlayerGetsOutOfPenaltyBox();
         }
 
         $this->game->movePlayer($this->roll->getValue());
         $this->game->askQuestion();
-    }
-
-    protected function displayPlayerRolls($rolledNumber)
-    {
-        $this->view->echoln("{$this->player->getName()} is the current player");
-        $this->view->echoln("They have rolled a {$rolledNumber}");
-    }
-
-    protected function displayPlayerStaysInPenaltyBox()
-    {
-        $this->view->echoln("{$this->player->getName()} is not getting out of the penalty box");
-    }
-
-    protected function displayPlayerGetsOutOfPenaltyBox()
-    {
-        $this->view->echoln("{$this->player->getName()} is getting out of the penalty box");
     }
 }
--- php/src/TurnView.php
+++ php/src/TurnView.php
@@ -4,15 +4,31 @@ namespace App;
 
 class TurnView
 {
-    private $turn;
+    private $player;
 
-    public function __construct(Turn $turn)
+    public function __construct(Player $player)
     {
-        $this->turn = $turn;
+        $this->player = $player;
     }
 
     public function echoln($string)
     {
         echo $string . "\n";
+    }
+
+    public function displayPlayerRolls($rolledNumber)
+    {
+        $this->echoln("{$this->player->getName()} is the current player");
+        $this->echoln("They have rolled a {$rolledNumber}");
+    }
+
+    public function displayPlayerStaysInPenaltyBox()
+    {
+        $this->echoln("{$this->player->getName()} is not getting out of the penalty box");
+    }
+
+    public function displayPlayerGetsOutOfPenaltyBox()
+    {
+        $this->echoln("{$this->player->getName()} is getting out of the penalty box");
     }
 }
